f65cbde41398b49084cd064d8b0eecb3077c663b
Nimira43 <n.rabone72@aol.com>
2026-03-25 12:56:36 +0000

    First commit

---
+++ js/creatures.js
@@ -0,0 +1,45 @@
+export const CREATURES = [
+  // --- Humanoids ---
+  { name: 'Dwarf', type: 'Humanoid', alignment: 'Good' },
+  { name: 'Elf', type: 'Humanoid', alignment: 'Good' },
+  { name: 'Hobbit', type: 'Humanoid', alignment: 'Good' },
+  { name: 'Warrior', type: 'Humanoid', alignment: 'Neutral' },
+  { name: 'Archer', type: 'Humanoid', alignment: 'Neutral' },
+  { name: 'Ranger', type: 'Humanoid', alignment: 'Good' },
+  { name: 'Knight', type: 'Humanoid', alignment: 'Good' },
+  { name: 'Centaur', type: 'Humanoid', alignment: 'Neutral' },
+  { name: 'Faun', type: 'Humanoid', alignment: 'Good' },
+  { name: 'Goblin', type: 'Humanoid', alignment: 'Evil' },
+  { name: 'Broo', type: 'Humanoid', alignment: 'Evil' },
+
+  // --- Small Magical Folk ---
+  { name: 'Pixie', type: 'Fey', alignment: 'Good' },
+
+  // --- Beasts & Mythic Creatures ---
+  { name: 'Pegasus', type: 'Beast', alignment: 'Good' },
+  { name: 'Unicorn', type: 'Beast', alignment: 'Good' },
+  { name: 'Griffin', type: 'Beast', alignment: 'Good' },
+
+  // --- Monsters ---
+  { name: 'Giant', type: 'Monster', alignment: 'Neutral' },
+  { name: 'Troll', type: 'Monster', alignment: 'Evil' },
+  { name: 'Ogre', type: 'Monster', alignment: 'Evil' },
+  { name: 'Orc', type: 'Monster', alignment: 'Evil' },
+  { name: 'Hydra', type: 'Monster', alignment: 'Evil' },
+
+  // --- Mythic Horrors ---
+  { name: 'Manticore', type: 'Mythic', alignment: 'Evil' },
+  { name: 'Harpy', type: 'Mythic', alignment: 'Evil' },
+
+  // --- Undead ---
+  { name: 'Demon', type: 'Undead', alignment: 'Evil' },
+  { name: 'Wraith', type: 'Undead', alignment: 'Evil' },
+  { name: 'Spectre', type: 'Undead', alignment: 'Evil' },
+  { name: 'Ghost', type: 'Undead', alignment: 'Neutral' },
+  { name: 'Vampire', type: 'Undead', alignment: 'Evil' },
+
+  // --- Dragons ---
+  { name: 'Green Dragon', type: 'Dragon', alignment: 'Neutral' },
+  { name: 'Gold Dragon', type: 'Dragon', alignment: 'Good' },
+  { name: 'Red Dragon', type: 'Dragon', alignment: 'Evil' }
+]
---
+++ js/main.js
@@ -0,0 +1,75 @@
+import { CREATURES } from './creatures.js'
+
+const MAP_WIDTH = 32
+const MAP_HEIGHT = 32
+const TILE_SIZE = 48
+const VIEW_TILES = 15
+const VIEW_RADIUS = Math.floor(VIEW_TILES / 2)
+
+const terrainColours = {
+  grass: '#3cb043',
+  rough: '#8b5a2b',
+  water: '#1e90ff'
+}
+
+console.log(CREATURES)
+
+const canvas = document.getElementById('map')
+canvas.width = VIEW_TILES * TILE_SIZE
+canvas.height = VIEW_TILES * TILE_SIZE
+const ctx = canvas.getContext('2d')
+
+function wrap(n, max) {
+  return (n + max) % max;
+}
+
+const map = []
+for (let y = 0; y < MAP_HEIGHT; y++) {
+  map[y] = []
+  for (let x = 0; x < MAP_WIDTH; x++) {
+    const r = Math.random()
+    if (r < 0.7) map[y][x] = 'grass'
+    else if (r < 0.9) map[y][x] = 'rough'
+    else map[y][x] = 'water'
+  }
+}
+
+let playerX = Math.floor(MAP_WIDTH / 2)
+let playerY = Math.floor(MAP_HEIGHT / 2)
+
+function drawViewport() {
+  for (let vy = 0; vy < VIEW_TILES; vy++) {
+    for (let vx = 0; vx < VIEW_TILES; vx++) {
+      const worldX = wrap(playerX + (vx - VIEW_RADIUS), MAP_WIDTH)
+      const worldY = wrap(playerY + (vy - VIEW_RADIUS), MAP_HEIGHT)
+
+      ctx.fillStyle = terrainColours[map[worldY][worldX]];
+      ctx.fillRect(
+        vx * TILE_SIZE,
+        vy * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE
+      )
+    }
+  }
+
+  ctx.fillStyle = 'white'
+  ctx.fillRect(
+    VIEW_RADIUS * TILE_SIZE,
+    VIEW_RADIUS * TILE_SIZE,
+    TILE_SIZE,
+    TILE_SIZE
+  )
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowUp') playerY = wrap(playerY - 1, MAP_HEIGHT)
+  else if (e.key === 'ArrowDown') playerY = wrap(playerY + 1, MAP_HEIGHT)
+  else if (e.key === 'ArrowLeft') playerX = wrap(playerX - 1, MAP_WIDTH)
+  else if (e.key === 'ArrowRight') playerX = wrap(playerX + 1, MAP_WIDTH)
+  else return
+
+  drawViewport()
+})
+
+drawViewport()
